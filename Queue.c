9d97671895e0b4f4c8e8dae023385256d8a220cb
FARDEEN <firdoshshaikh771@gmail.com>
2026-03-02 21:55:17 +0530

    Queue little change V2

--- Queue.c
+++ Queue.c
@@ -5,33 +5,45 @@ int front = -1;
 int rear = -1;
 
 void enqueue(int a){
-    if(rear == N -1){
-        printf("overflow!\n");
+    if(rear == N - 1){
+        printf("Overflow!\n");
         return;
     }
     else if(front == -1 && rear == -1){
         front = rear = 0;
         queue[rear] = a;
     }
-    else {
+    else{
         rear++;
         queue[rear] = a;
     }
 }
 
 void dequeue(){
-    int x;
     if(front == -1 && rear == -1){
         printf("Underflow!\n");
         return;
     }
     else if(front == rear){
-        printf("The poped item is: %d\n", queue[front]);
+        printf("Item poped: %d\n", queue[front]);
         front = rear = -1;
     }
     else {
-        printf("The poped item is: %d\n", queue[front]);
+        printf("Item poped: %d\n", queue[front]);
         front++;
+    }
+}
+
+void display(){
+    if(front == -1 && rear == -1){
+        printf("Underflow!\n");
+        return;
+    }
+    else{
+        printf("Items in Queue:\n");
+        for(int i = front; i <= rear; i++){
+            printf("%d\n", queue[i]);
+        }
     }
 }
 
@@ -41,32 +53,17 @@ void peek(){
         return;
     }
     else {
-        printf("The top most element: %d\n", queue[front]);
+        printf("The Top most element in Queue: %d\n", queue[front]);
     }
 }
 
-void display(){
-    if(front == -1 && rear == -1){
-        printf("Underflow!\n");
-        return;
-    }
-    else {
-        printf("The items in queue:\n");
-        for(int i = front; i <= rear; i++){
-            printf("%d\n", queue[i]);
-        }
-    }
-}
-
-int main(){
+void main(){
     enqueue(2);
     enqueue(5);
-    enqueue(-1);
+    enqueue(-8);
     display();
     peek();
     dequeue();
     peek();
     display();
-
-    return 0;
 }
