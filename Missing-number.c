202f6c81100fd132e21aa10bcfad9fea0302b5d7
FARDEEN <firdoshshaikh771@gmail.com>
2026-04-08 19:29:41 +0530

    Initial commit 2

--- Missing-number.c
+++ Missing-number.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 
 int main(){
-    int arr[] = {1, 2, 3, 5};
-    int Expected_sum = 0;
-    int Actual_sum = 0;
+    int expectedsum = 0;
+    int actualsum = 0;
+    int s = 0;
 
-    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Enter the size of array: ");
+    scanf("%d", &s);
 
-    int n = size + 1;
-
-    Expected_sum = n * (n + 1) / 2;
-
-    for(int i = 0; i < size; i++){
-        Actual_sum = Actual_sum + arr[i];
+    if(s <= 0){
+        printf("Array Size cant be in negative/Zero\n");
+        return 0;
     }
 
-    int result = Expected_sum - Actual_sum;
+    int arr[s];
+
+
+    printf("Enter the elements:");
+    for(int i = 0; i < s; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < s; i++){
+        for(int j = 0; j < i; j++){
+            if(arr[i] == arr[j]){
+                printf("There is duplicate number in array %d\n", arr[i]);
+                return 0;
+            }
+        }
+    }
+
+    int n = s + 1;
+
+    expectedsum = n * (n + 1) / 2;
+
+    for(int i = 0; i < s; i++){
+        actualsum = actualsum + arr[i];
+    }
+
+    int result = expectedsum - actualsum;
 
     printf("The missing element in array: %d\n", result);
 
